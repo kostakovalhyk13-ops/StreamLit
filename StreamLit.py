f04ff797b17f2f6038c87246dbb6df108155cfa7
kostakovalhyk13-ops <kostakovalhyk13@gmail.com>
2025-10-23 15:00:47 +0300

    Update StreamLit.py

--- StreamLit.py
+++ StreamLit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 
 st.set_page_config(page_title="Успішність студентів", layout="wide")
 
